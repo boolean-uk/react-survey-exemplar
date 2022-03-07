4dacbce83836cdbe38e6e824c551d1942553cebd
phillipdunn <phillipdunn@hotmail.com>
2022-03-07 15:09:22 +0000

    add answer form

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import AnswersList from './AnswersList';
+import AnswerForm from './AnswerForm';
+
 function Main() {
   const [open, setOpen] = useState(false); //Ignore this state
   const [answersList, setanswersList] = useState([]);
@@ -9,7 +11,7 @@ function Main() {
         <h2>Answers list</h2>
         <AnswersList answersList={answersList} />
       </section>
-      <section className="main__form">{/* a form should be here */}</section>
+      <section className="main__form">{<AnswerForm />}</section>
     </main>
   );
 }
