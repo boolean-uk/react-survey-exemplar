77eeb39c678ea0994b6a7cec2a339c11fb5879b6
phillipdunn <phillipdunn@hotmail.com>
2022-03-07 17:36:02 +0000

    add submit form logic

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,14 +4,21 @@ import AnswerForm from './AnswerForm';
 
 function Main() {
   const [open, setOpen] = useState(false); //Ignore this state
-  const [answersList, setanswersList] = useState([]);
+  const [answersList, setAnswersList] = useState([]);
+
+  const submitForm = (e, answers) => {
+    e.preventDefault();
+    setOpen(true);
+    setAnswersList((preArray) => [...preArray, answers]);
+  };
+
   return (
     <main className="main">
       <section className={`main__list ${open ? 'open' : ''}`}>
         <h2>Answers list</h2>
         <AnswersList answersList={answersList} />
       </section>
-      <section className="main__form">{<AnswerForm />}</section>
+      <section className="main__form">{<AnswerForm submitForm={submitForm} />}</section>
     </main>
   );
 }
