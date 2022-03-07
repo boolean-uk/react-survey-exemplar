9f9a41c481bcb5d756d4291750a6149b983fd36f
phillipdunn <phillipdunn@hotmail.com>
2022-03-07 15:02:34 +0000

    Add answerslist

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
-
+import { useState } from 'react';
+import AnswersList from './AnswersList';
 function Main() {
   const [open, setOpen] = useState(false); //Ignore this state
-
+  const [answersList, setanswersList] = useState([]);
   return (
     <main className="main">
-      <section className={`main__list ${open ? "open" : ""}`}>
+      <section className={`main__list ${open ? 'open' : ''}`}>
         <h2>Answers list</h2>
-        {/* answers should go here */}
+        <AnswersList answersList={answersList} />
       </section>
       <section className="main__form">{/* a form should be here */}</section>
     </main>
